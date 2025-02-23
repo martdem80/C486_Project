e88bc5db6cc4ca5695fabd3653a6867e6388dc7c
martdem80 <130099060+martdem80@users.noreply.github.com>
2025-02-23 00:52:40 -0500

    Update app.py
    Made changes to code

--- AppDev/app.py
+++ AppDev/app.py
@@ -1,6 +1,23 @@
-from PyQt5.QtCore import QSize, Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtCore import QSize, Qt, QProcess, QRect
+from PyQt5.QtGui import QIcon, QColor, QPalette, QFont 
+from PyQt5.QtWidgets import(
+    QApplication, 
+    QWidget, 
+    QMainWindow, 
+    QPushButton, 
+    QMainWindow,
+    QPushButton,
+    QToolBar,
+    QLabel,
+    QLineEdit,
+    QMenu,
+    QMenuBar,
+    QStatusBar,
+    QAction, 
+    QVBoxLayout, QHBoxLayout,
+    QListView,
 
+)
 import sys
 
 class MainWindow(QMainWindow):
@@ -9,11 +26,67 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("ASL interpreter")
 
-        button = QPushButton("Press here!")
-        
-        self.setCentralWidget(button)
+      #  button = QPushButton("Press here!")
 
-        self.setMinimumSize(QSize(400,300))
+        self.actionSave_as = QAction(MainWindow)
+        self.actionSave_as.setObjectName("actionSave_as")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.hello_label = QLabel(self.centralwidget)
+        self.hello_label.setObjectName("hello_label")
+        self.hello_label.setGeometry(QRect(130, 10, 421, 41))
+        font = QFont()
+        font.setItalic(True)
+        self.hello_label.setFont(font)
+        self.image_label = QLabel(self.centralwidget)
+        self.image_label.setObjectName("image_label")
+        self.image_label.setGeometry(QRect(130, 120, 571, 311))
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setObjectName("verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(10, 10, 106, 431))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.newdraft = QPushButton(self.verticalLayoutWidget)
+        self.newdraft.setObjectName("newdraft")
+
+        self.verticalLayout.addWidget(self.newdraft)
+
+        self.moch1 = QPushButton(self.verticalLayoutWidget)
+        self.moch1.setObjectName("moch1")
+
+        self.verticalLayout.addWidget(self.moch1)
+
+        self.listView = QListView(self.verticalLayoutWidget)
+        self.listView.setObjectName("listView")
+
+        self.verticalLayout.addWidget(self.listView)
+
+        self.userlineEdit = QLineEdit(self.centralwidget)
+        self.userlineEdit.setObjectName("userlineEdit")
+        self.userlineEdit.setGeometry(QRect(30, 450, 771, 101))
+        font1 = QFont()
+        font1.setPointSize(14)
+        self.userlineEdit.setFont(font1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName("menubar")
+        self.menubar.setGeometry(QRect(0, 0, 844, 22))
+        self.menuFile = QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuAbout = QMenu(self.menubar)
+        self.menuAbout.setObjectName("menuAbout")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuFile.menuAction())
+        self.menubar.addAction(self.menuAbout.menuAction())
+        self.menuFile.addAction(self.actionSave_as) 
+        
+
+        self.setMinimumSize(QSize(844,600))
 
 
 
